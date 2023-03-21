cb4cee45f2ffa3afc8c94f3a6e13e7d036e33458
Chiridev <chirino.dev@gmail.com>
2023-03-21 20:40:34 -0300

    todos los cambios de estilos para la botonera

--- JS/Conexion.js
+++ JS/Conexion.js
@@ -1,7 +1,21 @@
-$.ajax({
-    url: 'https://randomuser.me/api/',
-    dataType: 'json',
-    success: function(data) {
-      console.log(data);
-    }
-  });
+
+
+
+ const conexion = fetch('https://randomuser.me/api/')
+                  .then(response => response.json())
+                  .then(json => console.log(json))
+                 
+const age = conexion.find('age')
+
+console.log(JSON.strigify(age))
+  
+
+                 
+
+// const datos = conexion['results']
+
+
+// console.log(datos)
+
+// const data = JSON.stringify(datos)
+// console.log(data)
--- JS/Funciones.js
+++ JS/Funciones.js
@@ -1,1 +1,33 @@
-const operacion = require("./Conexion.js")
+'use strict'
+
+const li        = document.querySelectorAll('.li')
+const bloque    = document.querySelectorAll('.bloque')
+const boton    = document.querySelectorAll('.boton')
+
+
+
+li.forEach( ( cadaLi , i )=>{
+    
+    li[i].addEventListener('click',()=>{
+
+        
+        li.forEach( ( cadaLi , i )=>{
+            
+            li[i].classList.remove('activo')
+            bloque[i].classList.remove('activo')
+            boton[i].classList.remove('activo')
+        })
+
+        
+        li[i].classList.add('activo')
+        bloque[i].classList.add('activo')
+        boton[i].classList.add('activo')
+
+    })
+});
+
+
+
+
+
+
